ad2a5d4917a7177ef38060a964917629dad5b568
Maria Schreiber <meschreiber3@gmail.com>
2017-04-18 16:24:39 -0400

    created jointable

--- db/models/orderItem.js
+++ db/models/orderItem.js
@@ -4,9 +4,9 @@ module.exports = db => db.define('orderItems', {
   quantity: INTEGER,
 })
 
-module.exports.associations = (OrderItem, {Product, Order}) => {
-  OrderItem.belongsTo(Order)
-  OrderItem.hasOne(Product)
-}
+// module.exports.associations = (OrderItem, {Product, Order}) => {
+//   OrderItem.belongsTo(Order)
+//   OrderItem.hasOne(Product)
+// }
 
 // Does sequelize store the previous values of an object?
