425320742041491b5b27569dfa3e912dd5116bd8
Fanny Jiang <jiangf@bu.edu>
2017-04-19 16:07:19 -0400

    new branch

---
+++ app/components/App.jsx
@@ -0,0 +1,18 @@
+'use strict'
+import React from 'react'
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
+import { render } from 'react-dom'
+import { connect, Provider } from 'react-redux'
+
+export const App = connect(
+  ({ auth }) => ({ user: auth })
+)(
+  ({ user, children }) =>
+    <div>
+      <nav>
+        {user ? <WhoAmI /> : <Login />}
+        <h1>HELLO</h1>
+      </nav>
+      {children}
+    </div>
+  )
