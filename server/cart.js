65c808e862a7c9a8ad232358fb2cbaad872c6d16
ssundby <stefanie.sundby@gmail.com>
2017-04-20 16:03:50 -0400

    created GET, POST, DELETE, PUT api routes for cart

--- server/cart.js
+++ server/cart.js
@@ -8,15 +8,54 @@ const User = db.model('users') // for orders by a user
 const { mustBeLoggedIn, forbidden, selfOnly } = require('./auth.filters')
 
 module.exports = require('express').Router()
+
+  // GET / sends back the cart, which is an instance of Order
   .get('/',
   (req, res, next) =>
     res.send(req.cart)
   )
+
+  // POST /:productId adds a new item to the cart
+  // We assume that req.body will include item quantity
+  // NOTE: this is untested -- don't know how to test this in Postman
   .post('/:productId',
   (req, res, next) => {
-    const product = Product.findById(req.params.productId)
-    req.cart.addProduct(product)
-    res.json(req.cart)
+    Product.findById(req.params.productId)
+    .then(product => req.cart.addProduct(product))
+    .then((cart) => {
+      OrderItem.update(
+        { quantity: req.body.quantity },
+        { where: { order_id: cart.id } },
+        { returning: true })
+    })
+    .then(orderItem => res.send({
+      message: 'Item successfully added to cart',
+      item: orderItem[1][0]
+    }))
+    .catch(next)
   })
-  // We don't know how to test this using postman or other methods
 
+  // DELETE /:productId deletes* an item from our cart
+  // NOTE: if this doesn't work well, we change item quantity to 0 instead
+  .delete('/:productId',
+  (req, res, next) => {
+    req.cart.destroy(
+      { model: OrderItem },
+      { where: { product_id: req.params.productId } })
+      .catch(next)
+  })
+
+  // PUT /:productId changes the quantity of an item in cart
+  .put('/:productId',
+  (req, res, next) => {
+    req.cart.update(
+      { quantity: req.body.quantity },
+      { model: OrderItem },
+      { where: { product_id: req.params.productId } },
+      { returning: true })
+    .then(orderItem => res.send({
+      message: 'Modified quantity',
+      item: orderItem[1][0]
+    }))
+    .catch(next)
+  })
