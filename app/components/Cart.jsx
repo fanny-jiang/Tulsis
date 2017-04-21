61b618b1c173fff2d2b42e85d600d95f5ac3e110
Fanny Jiang <jiangf@bu.edu>
2017-04-21 12:04:34 -0400

    fixed cart props rendering issue, order items now rendering

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -3,13 +3,22 @@ import { Link } from 'react-router'
 
 
 export default function Cart(props) {
-  const cart = props
+  const cart = props.cart || [] // cart component is rendering before props had been received, set cart to be an empty array by default so that code doesn't break when it renders the first time around
   console.log('PROPS FROM CART: ', cart)
   return (
     <div>
       <h3> CART!!! </h3>
       <div>
-
+        {
+          cart.map((orderItem) => (
+            <ul key={`orderItem.id`}>
+              <li><img src={`orderItem.product.photoUrl`}></img></li>
+              <li>{orderItem.product.title}</li>
+              <li>{orderItem.quantity}</li>
+              <li>{orderItem.product.price}</li>
+            </ul>
+          ))
+        }
       </div>
     </div>
   )
