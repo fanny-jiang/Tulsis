01df92e2371cdd5c303ed80cc385b70922c13cb7
ssundby <stefanie.sundby@gmail.com>
2017-04-21 15:29:53 -0400

    omg closing this branch for real. spotted another problem

--- app/containers/CartContainer.jsx
+++ app/containers/CartContainer.jsx
@@ -2,28 +2,6 @@ import Cart from '../components/Cart'
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
 
-class CartContainer extends Component {
-  constructor(props) {
-    super(props)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit(evt) {
-    evt.preventDefault()
-    axios.post(`/api/cart/${evt.target.value}`, { quantity: 1 })
-    .catch(err => console.error('Cannot add product to cart', err))
-  }
-
-  render() {
-    return (
-      <Catalog products={this.props.products} onSubmit={this.handleSubmit} />
-    )
-  }
-}
-
-
-
-
 const mapStateToProps = (state) => {
   console.log('cart container state', state)
   return {
