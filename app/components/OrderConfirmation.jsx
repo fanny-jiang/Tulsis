7f295f29920297b83d3ecccf255fa497d137b825
ssundby <stefanie.sundby@gmail.com>
2017-04-21 12:52:34 -0400

    added an api route for confirmation page.. maybe unncessarily

--- app/components/OrderConfirmation.jsx
+++ app/components/OrderConfirmation.jsx
@@ -3,13 +3,9 @@ import React, { Component } from 'react'
 export default function ConfirmationPage() {
   return (
     <div>
+        <br/>
+        <br/>
         <h1 style={{textAlign: 'center'}}>Thank you for your order!</h1>
-        <div style={{
-          borderStyle: 'solid',
-          borderWidth: '0.5px',
-          padding: '20px'
-        }}>
-        </div>
     </div>
   )
 }
--- server/cart.js
+++ server/cart.js
@@ -54,3 +54,9 @@ module.exports = require('express').Router()
     }))
     .catch(next)
   })
+
+  // so that order confirmation page shows up... this is necessary, right?
+  .get('/confirmation',
+  (req, res, next) => {
+    res.sendStatus(200)
+  })
