7b4267458b75fb04d46ce5ce7a48e40c9dfbf683
ssundby <stefanie.sundby@gmail.com>
2017-04-23 16:19:25 -0400

    styled WhoAmI and Login components

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -11,17 +11,16 @@ export default function Navbar(props) {
       <div className="navbar-user-menu">
           { props.user
             ? <WhoAmI />
-            : <span>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
-              </span> }
+            : <Login /> }
         <Link to="/cart">
           <img src="/images/cart_icon.jpeg" className="icon" />
         </Link>
       </div>
 
       {/* topmost block element with store name */}
-      <div className="navbar-title">Babies</div>
+      <div className="navbar-title">
+        <Link to="/home">Babies</Link>
+      </div>
 
       {/* bottom block element with navigation links */}
       <div className="navbar-bottom">
@@ -34,44 +33,3 @@ export default function Navbar(props) {
     </div>
   )
 }
-
-
-
-
-// PREVIOUS JSX:
-
-/*
-<div>
-  <div className="container-fluid">
-    <div>
-      <ul className="nav navbar-nav navbar-right float_right">
-        <li>{props.user ? <WhoAmI /> : <Login />}</li>
-        <li>
-          <Link to="/order">
-            <img src='../../public/images/cart_icon.png' />
-          </Link>
-        </li>
-      </ul>
-      <h1>
-        <Link className="navbar-brand" to="/home">TULSI'S BABY BOUTIQUE</Link>
-      </h1>
-    </div>
-  </div>
-  <div className="container-fluid">
-    <ul className="nav justify-content-center">
-      <li className="nav-item float_right">
-        <Link to="/catalog" >All</Link>
-      </li>
-      <li className="nav-item float_right">
-        <Link to="/catalog/newborns">Newborns</Link>
-      </li>
-      <li className="nav-item float_right">
-        <Link to="/catalog/infants">Infants</Link>
-      </li>
-      <li className="nav-item float_right">
-        <Link to="/catalog/toddlers">Toddlers</Link>
-      </li>
-    </ul>
-  </div>
-</div>
-*/
