0828a5b4d4269bdb6301b3aff7233c4a07c12963
Maria Schreiber <meschreiber3@gmail.com>
2017-04-24 19:39:19 -0400

    click here to continue shopping

--- app/containers/CartContainer.jsx
+++ app/containers/CartContainer.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import axios from 'axios'
 
+
 const mapStateToProps = (state) => {
   return {
     cart: state.cart.orderItems
